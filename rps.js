f826beeab4b32d087d558862a556dad26571cefd
Sabrina <1771581+sabs@users.noreply.github.com>
2023-04-25 22:39:49 +0100

    basic conversion - buttons work, shows result

--- rps.js
+++ rps.js
@@ -24,35 +24,39 @@ function capitalize(str) {
 }
 
 function playRoundRPS(computer_choice, player_choice) {
-    console.log("Player choice " + player_choice);
-    console.log("Computer choice " + computer_choice);
+    displayMessage("");
+    let outputstring ="Player choice " + player_choice;
+    outputstring += "<br>Computer choice " + computer_choice;
+    outputstring += "<br>";
     if (player_choice === computer_choice) {
-        return "It's a draw!";
+        return displayMessage(outputstring + "It's a draw!");
+        
     }
     if (computer_choice === "Rock") {
         if (player_choice === "Paper") {
-            return `You win! ${player_choice} beats ${computer_choice}`;
+            return displayMessage(outputstring + `You win! ${player_choice} beats ${computer_choice}`);
         } else if (player_choice === "Scissors") {
-            return `You lose! ${player_choice} is defeated by ${computer_choice}`;
+            return displayMessage(outputstring + `You lose! ${player_choice} is defeated by ${computer_choice}`);
         } 
     }
     if (computer_choice === "Paper") {
         if (player_choice === "Scissors") {
-            return `You win! ${player_choice} beats ${computer_choice}`;
+            return displayMessage(outputstring + `You win! ${player_choice} beats ${computer_choice}`);
         } else if (player_choice === "Rock") {
-            return `You lose! ${player_choice} is defeated by ${computer_choice}`;
+            return displayMessage(outputstring + `You lose! ${player_choice} is defeated by ${computer_choice}`);
         } 
     }
     if (computer_choice === "Scissors") {
         if (player_choice === "Rock") {
-            return `You win! ${player_choice} beats ${computer_choice}`;
+            return displayMessage(outputstring + `You win! ${player_choice} beats ${computer_choice}`);
         } else if (player_choice === "Paper") {
-            return `You lose! ${player_choice} is defeated by ${computer_choice}`;
+            return displayMessage(outputstring + `You lose! ${player_choice} is defeated by ${computer_choice}`);
         } 
     }
 }
 
 function getPlayerChoice() {
+    // not used in GUI version
     const rps_values = ["Rock", "Paper", "Scissors"];
     while (true) {
         let pchoice = capitalize(prompt("Make your move: rock, paper or scissors?"));
@@ -64,10 +68,26 @@ function getPlayerChoice() {
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRoundRPS(getComputerChoice(), getPlayerChoice()));
-    }
+function displayMessage(str) {
+    document.getElementById('results').innerHTML = str;
 }
 
+function playRound(e) {
+    console.log(this.innerText);
+    playRoundRPS(getComputerChoice(), this.innerText);
+}
+
+function game() {
+
+    // console.log(playRoundRPS(getComputerChoice(), getPlayerChoice()));
+
+}
+
+function setUpButtons() {
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach(btn => btn.addEventListener('click', playRound)); 
+}
+
+setUpButtons();
 game();
